32a64ca2e7a855945b6a9de5fb3799b924b9b514
Christopher May <cmay3249@gmail.com>
2025-08-15 21:59:10 -0700

    basic taskpane appears

--- excel-get-started-with-dev-kit/src/taskpane/taskpane.js
+++ excel-get-started-with-dev-kit/src/taskpane/taskpane.js
@@ -3,31 +3,12 @@
  * See LICENSE in the project root for license information.
  */
 
-/* global console, document, Excel, Office */
+/* global document, Office */
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
     document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+  document.getElementById("app-body").classList.remove("is-hidden");
   }
 });
 
-export async function run() {
-  try {
-    await Excel.run(async (context) => {
-      const range = context.workbook.getSelectedRange();
-
-      // Read the range address.
-      range.load("address");
-
-      // Update the fill color.
-      range.format.fill.color = "yellow";
-
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
